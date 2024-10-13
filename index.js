924a305f128a7ef1e87074a6cea3eed2783d11f2
rshdwantstocode <rushseed02@gmail.com>
2024-10-13 10:48:22 +0800

    added minute and tested the second function in js

---
+++ index.js
@@ -0,0 +1,10 @@
+const Hours = document.querySelector(".hour_time")
+const Minutes = document.querySelector(".minute_time")
+const Seconds = document.querySelector(".second_time")
+
+
+
+const d = new Date();
+let seconds = d.getUTCSeconds();
+
+console.log();
