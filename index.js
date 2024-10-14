039ddeba79e2e83d1e1605557005f7704ae75675
rshdwantstocode <rushseed02@gmail.com>
2024-10-14 10:15:20 +0800

    set up the function for updating the clock

--- index.js
+++ index.js
@@ -1,10 +1,24 @@
-const Hours = document.querySelector(".hour_time")
-const Minutes = document.querySelector(".minute_time")
-const Seconds = document.querySelector(".second_time")
+const Hours = document.querySelector(".hour_time");
+const Minutes = document.querySelector(".minute_time");
+const Seconds = document.querySelector(".second_time");
 
 
 
-const d = new Date();
-let seconds = d.getUTCSeconds();
+function updateTime(){
+    const date = new Date();
+    let seconds = date.getSeconds();
+    let minutes = date.getMinutes();
+    let hours = date.getHours();
 
-console.log("kupal");
+    let hours_rotate = (hours % 12) * 30 + (minutes / 60) * 30;
+    let mins_rotate = minutes * 6;
+    let sec_rotate = seconds * 6;
+
+    Hours.style.transform = `rotate(${hours_rotate}deg)`;
+    Minutes.style.transform = `rotate(${mins_rotate}deg)`;
+    Seconds.style.transform = `rotate(${sec_rotate}deg)`;
+};
+
+setInterval(updateTime, 1000);
+
+updateTime();
